a9c10b0a767357bae07f7b08f8b0f89e1a595baf
Joao Adherval <joaoadherval@gmail.com>
2021-08-25 18:07:54 -0300

    creating /api/whoami endpoint and getting json paramerters accordingly from the request

--- server.js
+++ server.js
@@ -25,9 +25,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get("/api/whoami", function (req, res) {
+  let response = {};
+
+  ip = req.connection.remoteAddress;
+  language = req.headers["accept-language"];
+  software = req.headers["user-agent"];
+
+  response["ipaddress"] = ip;
+  response["language"] = language;
+  response["software"] = software;
+
+  res.json(response);
 });
